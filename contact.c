f6faeecc36b4c848b650b8536b0aebe76559eef2
phinehas <phinehasmane2@gmail.com>
2022-12-27 10:47:19 +0530

    Added more c

---
+++ contact.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
